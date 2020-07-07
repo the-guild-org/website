dfb056dcc5c690338a25d6110866588d11fba5a8
Dotan Simha <dotansimha@gmail.com>
2020-07-07 18:58:05 +0300

    added segments by domain

--- public/static/banner.js
+++ public/static/banner.js
@@ -47,6 +47,20 @@ function init() {
   document.body.prepend(container);
 }
 
+const HOST_TO_SEGMENTS = {
+  'graphql-code-generator.com': ['codegen'],
+  'graphql-modules.com': ['modules'],
+  'graphql-tools.com': ['tools'],
+  'the-guild.dev': ['guild-website'],
+  'graphql-inspector.com': ['inspector'],
+  'tortilla.academy': ['tortilla'],
+  'graphql-mesh.com': ['mesh'],
+};
+
 if (typeof window !== 'undefined') {
   window.$crisp = []; window.CRISP_WEBSITE_ID = "af9adec5-ddfa-4db9-a4a3-25769daf2fc2"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
+
+  if (HOST_TO_SEGMENTS[window.location.host]) {
+    window.$crisp.push(["set", "session:segments", [HOST_TO_SEGMENTS[window.location.host]]])
+  }
 }
