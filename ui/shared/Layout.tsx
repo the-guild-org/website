8251f6e64259db2660014bc434039db0fa7350f1
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-08-19 12:30:36 +0200

    Fix svg attrs

--- ui/shared/Layout.tsx
+++ ui/shared/Layout.tsx
@@ -328,9 +328,9 @@ export const Arrow: React.FC<{ className?: string }> = ({ className }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
     >
       <polyline points="20 6 9 17 4 12"></polyline>
