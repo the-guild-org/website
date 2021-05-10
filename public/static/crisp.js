f333725255c5941f20537114d7f1fa8ae37df5c2
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-05-10 17:31:19 +0200

    Fix crisp (#354)

--- public/static/crisp.js
+++ public/static/crisp.js
@@ -1,3 +1,21 @@
 if (typeof window !== 'undefined') {
-    window.$crisp = []; window.CRISP_WEBSITE_ID = "af9adec5-ddfa-4db9-a4a3-25769daf2fc2"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
+    const HOST_TO_SEGMENTS = {
+    'graphql-code-generator.com': ['codegen'],
+    'graphql-modules.com': ['modules'],
+    'graphql-tools.com': ['tools'],
+    'the-guild.dev': ['guild-website'],
+    'graphql-inspector.com': ['inspector'],
+    'tortilla.academy': ['tortilla'],
+    'graphql-mesh.com': ['mesh'],
+    'apollo-angular.com': ['angular'],
+    'graphql-cli.com': ['cli'],
+    'graphql-scalars.dev': ['scalars'],
+    'graphql-hive.com': ['hive']
+  };
+
+  window.$crisp = []; window.CRISP_WEBSITE_ID = "af9adec5-ddfa-4db9-a4a3-25769daf2fc2"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
+
+  if (HOST_TO_SEGMENTS[window.location.host]) {
+    window.$crisp.push(["set", "session:segments", [HOST_TO_SEGMENTS[window.location.host]]])
+  }
 }
