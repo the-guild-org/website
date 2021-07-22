f79bfeb798a551b184531770845383616c16de7b
Dotan Simha <dotansimha@gmail.com>
2021-07-22 12:05:48 +0300

    wip fix

--- blog-sync.mjs
+++ blog-sync.mjs
@@ -14,16 +14,19 @@ const DEV_TO_TOKEN = process.env.DEV_TO_TOKEN;
 const DEV_TO_ORG_ID = 4467;
 
 const baseDir = './pages/blog/';
-const files = globby.sync('*.mdx', {
-  absolute: false,
-  cwd: baseDir,
-});
+// const files = globby.sync('*.mdx', {
+//   absolute: false,
+//   cwd: baseDir,
+// });
+const files = ['better-type-safety-for-resolvers-with-graphql-codegen.mdx'];
 
 const processor = unified()
   .use(parse, { position: false })
   .use(stringify)
   .use(mdx)
   .use(extractMeta);
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function syncToDevTo(items) {
   console.log(`=== Syncing ${items.length} articles to dev.to... ===`);
@@ -36,7 +39,12 @@ async function syncToDevTo(items) {
   for (const item of items) {
     const exists = allArticles.find((t) => t.canonicalUrl === item.canonical);
     const author = details.authors[item.meta.author];
-    const markdown = `> This article was published on ${item.meta.date} by [${author.name}](${author.link}) @ [The Guild Blog](https://the-guild.dev/)\n\n${item.markdown}`;
+    const markdown = `> This article was published on ${item.meta.date} by [${author.name}](${author.link}) @ [The Guild Blog](https://the-guild.dev/)\n\n${item.markdown} `;
+    const image = item.meta.image
+      ? item.meta.image.startsWith('/')
+        ? `https://the-guild.dev${item.meta.image}`
+        : item.meta.image
+      : undefined;
 
     if (exists) {
       console.log(
@@ -52,14 +60,11 @@ async function syncToDevTo(items) {
             bodyMarkdown: markdown,
             canonicalUrl: item.meta.canonical || item.canonical,
             description: item.meta.description,
-            mainImage: item.meta.image
-              ? item.meta.image.startsWith('/')
-                ? `https://the-guild.dev${item.meta.image}`
-                : item.meta.image
-              : undefined,
+            mainImage: image,
             tags: (item.meta.tags || []).map((t) => t.replace(/[-_ ]/g, '')),
           },
         });
+        await sleep(10000);
         console.log(`   -> Done!`);
       } else {
         console.log(`   -> Up to date!`);
@@ -70,19 +75,16 @@ async function syncToDevTo(items) {
       await client.createArticle({
         article: {
           title: item.meta.title,
-          bodyMarkdown: item.markdown,
+          bodyMarkdown: markdown,
           canonicalUrl: item.meta.canonical || item.canonical,
           description: item.meta.description,
           organizationId: DEV_TO_ORG_ID,
-          mainImage: item.meta.image
-            ? item.meta.image.startsWith('/')
-              ? `http://the-guild.dev${item.meta.image}`
-              : item.meta.image
-            : undefined,
+          mainImage: image,
           published: true,
           tags: (item.meta.tags || []).map((t) => t.replace(/[-_ ]/g, '')),
         },
       });
+      await sleep(10000);
     }
   }
 }
@@ -105,7 +107,7 @@ async function main() {
         });
       }
     } catch (e) {
-      console.error(`Building clean MD failed for "${blogFile}":`, e);
+      console.error(`Building clean MD failed for "${blogFile}"...`);
       throw e;
     }
   }
@@ -119,7 +121,7 @@ function extractMeta() {
   return (tree, file) => {
     const meta = {};
 
-    visit(tree, (node) => {
+    visit(tree, (node, index, parent) => {
       if (node.type === 'mdxjsEsm') {
         if (node.value.includes('export const meta')) {
           walk(node.data.estree, {
@@ -135,6 +137,28 @@ function extractMeta() {
         }
 
         return;
+      } else if (node.type === 'mdxJsxFlowElement') {
+        if (node.name === 'Gfycat') {
+          const gifId = node.attributes
+            .find((a) => a.name === 'gifId')
+            .value.value.replace(/['"]/g, '');
+
+          parent.children.splice(index, 1, {
+            type: 'text',
+            value: `<img width="100%" style="width:100%" src="https://thumbs.gfycat.com/${gifId}-size_restricted.gif">`,
+            position: node.position,
+          });
+        } else if (node.name === 'CodeSandbox') {
+          const boxId = node.attributes
+            .find((a) => a.name === 'codeSandboxId')
+            .value.value.replace(/['"]/g, '');
+
+          parent.children.splice(index, 1, {
+            type: 'text',
+            value: `{% codesandbox ${boxId} %}`,
+            position: node.position,
+          });
+        }
       }
     });
 
