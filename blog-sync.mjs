328a8f25c3c9e524cd237aa971e8b29eb99cb415
Dotan Simha <dotansimha@gmail.com>
2021-07-29 11:51:56 +0300

    it's working

--- blog-sync.mjs
+++ blog-sync.mjs
@@ -14,11 +14,12 @@ const DEV_TO_TOKEN = process.env.DEV_TO_TOKEN;
 const DEV_TO_ORG_ID = 4467;
 
 const baseDir = './pages/blog/';
-// const files = globby.sync('*.mdx', {
-//   absolute: false,
-//   cwd: baseDir,
-// });
-const files = ['better-type-safety-for-resolvers-with-graphql-codegen.mdx'];
+const files = globby.sync('*.mdx', {
+  absolute: false,
+  cwd: baseDir,
+});
+
+// const files = ['codegen-typescript-react-apollo.mdx'];
 
 const processor = unified()
   .use(parse, { position: false })
@@ -37,54 +38,68 @@ async function syncToDevTo(items) {
   });
 
   for (const item of items) {
-    const exists = allArticles.find((t) => t.canonicalUrl === item.canonical);
-    const author = details.authors[item.meta.author];
-    const markdown = `> This article was published on ${item.meta.date} by [${author.name}](${author.link}) @ [The Guild Blog](https://the-guild.dev/)\n\n${item.markdown} `;
-    const image = item.meta.image
-      ? item.meta.image.startsWith('/')
-        ? `https://the-guild.dev${item.meta.image}`
-        : item.meta.image
-      : undefined;
+    try {
+      const exists = allArticles.find((t) => t.canonicalUrl === item.canonical);
+      const author = details.authors[item.meta.author];
+      const markdown = `> This article was published on ${item.meta.date} by [${author.name}](${author.link}) @ [The Guild Blog](https://the-guild.dev/)\n\n${item.markdown} `;
+      const image = item.meta.image
+        ? item.meta.image.startsWith('/')
+          ? `https://the-guild.dev${item.meta.image}`
+          : item.meta.image
+        : undefined;
+      const canonicalUrl = item.meta.canonical || item.canonical;
+      const tags = (item.meta.tags || [])
+        .map((t) => t.replace(/[-_ ]/g, ''))
+        .slice(0, 4);
 
-    if (exists) {
-      console.log(
-        `Article "${item.meta.title}" already exists, updating if needed...`
-      );
+      if (exists) {
+        console.log(
+          `Article "${item.meta.title}" already exists, updating if needed...`
+        );
 
-      if (exists.bodyMarkdown !== markdown) {
-        console.log(`   -> Updating...`);
+        if (exists.bodyMarkdown !== markdown) {
+          console.log(`   -> Updating...`);
 
-        await client.updateArticle(exists.id, {
+          await client.updateArticle(exists.id, {
+            article: {
+              title: item.meta.title,
+              bodyMarkdown: markdown,
+              canonicalUrl,
+              description: item.meta.description,
+              mainImage: image,
+              tags,
+            },
+          });
+          await sleep(10000);
+          console.log(`   -> Done!`);
+        } else {
+          console.log(`   -> Up to date!`);
+        }
+      } else {
+        console.log(`Creating article "${item.meta.title}" on DevTo...`);
+
+        await client.createArticle({
           article: {
             title: item.meta.title,
             bodyMarkdown: markdown,
-            canonicalUrl: item.meta.canonical || item.canonical,
+            canonicalUrl,
             description: item.meta.description,
+            organizationId: DEV_TO_ORG_ID,
             mainImage: image,
-            tags: (item.meta.tags || []).map((t) => t.replace(/[-_ ]/g, '')),
+            published: true,
+            tags,
           },
         });
         await sleep(10000);
-        console.log(`   -> Done!`);
-      } else {
-        console.log(`   -> Up to date!`);
       }
-    } else {
-      console.log(`Creating article "${item.meta.title}" on DevTo...`);
-
-      await client.createArticle({
-        article: {
-          title: item.meta.title,
-          bodyMarkdown: markdown,
-          canonicalUrl: item.meta.canonical || item.canonical,
-          description: item.meta.description,
-          organizationId: DEV_TO_ORG_ID,
-          mainImage: image,
-          published: true,
-          tags: (item.meta.tags || []).map((t) => t.replace(/[-_ ]/g, '')),
-        },
-      });
-      await sleep(10000);
+    } catch (e) {
+      console.log(
+        `Failed to send article to dev.to, error: `,
+        e.response.status,
+        e.response.statusText,
+        e.response.data
+      );
+      process.exit(1);
     }
   }
 }
@@ -102,7 +117,7 @@ async function main() {
         items.push({
           slug: cleanName,
           canonical: `https://the-guild.dev/blog/${cleanName}`,
-          markdown: value,
+          markdown: value.replace(/\\<img/gi, '<img'),
           meta: data.meta,
         });
       }
@@ -120,6 +135,7 @@ main();
 function extractMeta() {
   return (tree, file) => {
     const meta = {};
+    const embedOptions = {};
 
     visit(tree, (node, index, parent) => {
       if (node.type === 'mdxjsEsm') {
@@ -128,6 +144,17 @@ function extractMeta() {
             enter(node) {
               if (node.type === 'Property') {
                 meta[node.key.name] =
+                  node.value.type === 'ArrayExpression'
+                    ? node.value.elements.map((n) => n.value)
+                    : node.value.value;
+              }
+            },
+          });
+        } else if (node.value.includes('export const embedOptions')) {
+          walk(node.data.estree, {
+            enter(node) {
+              if (node.type === 'Property') {
+                embedOptions[node.key.name] =
                   node.value.type === 'ArrayExpression'
                     ? node.value.elements.map((n) => n.value)
                     : node.value.value;
@@ -145,19 +172,66 @@ function extractMeta() {
 
           parent.children.splice(index, 1, {
             type: 'text',
-            value: `<img width="100%" style="width:100%" src="https://thumbs.gfycat.com/${gifId}-size_restricted.gif">`,
+            value: `<img width="100%" style="width:100%" src="https://thumbs.gfycat.com/${gifId}-size_restricted.gif" />`,
             position: node.position,
           });
         } else if (node.name === 'CodeSandbox') {
           const boxId = node.attributes
             .find((a) => a.name === 'codeSandboxId')
             .value.value.replace(/['"]/g, '');
+          const childEmbedOptions = {};
+          const childEmbedOptionsNode = node.attributes.find(
+            (a) => a.name === 'embedOptions'
+          );
 
-          parent.children.splice(index, 1, {
-            type: 'text',
-            value: `{% codesandbox ${boxId} %}`,
-            position: node.position,
-          });
+          if (childEmbedOptionsNode) {
+            walk(childEmbedOptionsNode.value.data.estree, {
+              enter(node) {
+                if (node.type === 'Property') {
+                  childEmbedOptions[node.key.name] =
+                    node.value.type === 'ArrayExpression'
+                      ? node.value.elements.map((n) => n.value)
+                      : node.value.value;
+                }
+              },
+            });
+          }
+
+          const allEmbedOptions = {
+            ...embedOptions,
+            ...childEmbedOptions,
+          };
+
+          if (boxId.startsWith('github/')) {
+            const optionsQueryString = Object.keys(allEmbedOptions)
+              .map((k) => `${k}=${allEmbedOptions[k]}`)
+              .join('&');
+
+            parent.children.splice(index, 1, {
+              type: 'text',
+              value: `https://codesandbox.io/embed/${boxId}?${optionsQueryString}`,
+              position: node.position,
+            });
+          } else {
+            const optionsQueryString = Object.keys(allEmbedOptions)
+              .filter((k) =>
+                ['module', 'runonclick', 'initialpath'].includes(k)
+              )
+              .map((k) => {
+                const [item] = allEmbedOptions[k].split(',');
+
+                return `${k}=${item.startsWith('/') ? item.substr(1) : item}`;
+              })
+              .join(' ');
+
+            console.log(`{% codesandbox ${boxId} ${optionsQueryString} %}`);
+
+            parent.children.splice(index, 1, {
+              type: 'text',
+              value: `{% codesandbox ${boxId} ${optionsQueryString} %}`,
+              position: node.position,
+            });
+          }
         }
       }
     });
