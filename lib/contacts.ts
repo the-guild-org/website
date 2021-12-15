e8238e06ad7a9945b52ea6d5f4880d0e5b951f64
Dotan Simha <dotansimha@gmail.com>
2021-12-15 15:15:15 +0200

    fixes delete old pages

--- lib/contacts.ts
+++ lib/contacts.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable react-hooks/rules-of-hooks */
 /// @ts-check
 const Crisp = require('node-crisp-api');
 const produce = require('immer').default;
