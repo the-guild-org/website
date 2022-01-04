f55e120bf60fd58ccaed8d36b03c4091d6b70e2d
Dotan Simha <dotansimha@gmail.com>
2022-01-04 11:30:12 +0200

    remove crisp from banner file

--- public/static/banner.js
+++ public/static/banner.js
@@ -414,41 +414,6 @@ function main() {
     document.body.prepend(guildHeader);
     bindModal();
   }
-
-  const HOST_TO_SEGMENTS = {
-    'graphql-code-generator.com': ['codegen'],
-    'graphql-modules.com': ['modules'],
-    'graphql-tools.com': ['tools'],
-    'the-guild.dev': ['guild-website'],
-    'graphql-inspector.com': ['inspector'],
-    'tortilla.academy': ['tortilla'],
-    'graphql-mesh.com': ['mesh'],
-    'apollo-angular.com': ['angular'],
-    'graphql-cli.com': ['cli'],
-    'graphql-scalars.dev': ['scalars'],
-  };
-
-  if (typeof window !== 'undefined') {
-    /* eslint-disable */
-    window.$crisp = [];
-    window.CRISP_WEBSITE_ID = 'af9adec5-ddfa-4db9-a4a3-25769daf2fc2';
-    (function () {
-      d = document;
-      s = d.createElement('script');
-      s.src = 'https://client.crisp.chat/l.js';
-      s.async = 1;
-      d.getElementsByTagName('head')[0].appendChild(s);
-    })();
-    /* eslint-enable */
-
-    if (HOST_TO_SEGMENTS[window.location.host]) {
-      window.$crisp.push([
-        'set',
-        'session:segments',
-        [HOST_TO_SEGMENTS[window.location.host]],
-      ]);
-    }
-  }
 }
 
 main();
