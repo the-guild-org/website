355f3a92eaa49577890075fb7347e94fef9510b1
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-02-27 01:27:56 +0100

    move global.css to ./styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import 'prism-theme-night-owl/build/no-italics.css';
 import 'remark-admonitions/styles/classic.css';
 import * as gtag from '../lib/gtag';
 import { GA_TRACKING_ID } from '../lib/gtag';
-import './global.css';
+import '../styles/global.css';
 
 Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 
