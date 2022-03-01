8cc83bba0315d3c5202d6b3ddd511491da323ac7
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-03-01 11:25:39 +0100

    add unstable_includeFiles

--- pages/api/get-articles.ts
+++ pages/api/get-articles.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllArticles } from '../../lib/get-all-articles';
 
@@ -5,11 +7,15 @@ export const config = {
   api: {
     bodyParser: false,
   },
+  unstable_includeFiles: ['pages/blog'],
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const tags = Array.isArray(req.query.tags) ? req.query.tags : [];
+  console.log(process.cwd())
+  console.log(fs.readdirSync(process.cwd()))
+  console.log(fs.readdirSync(path.join(process.cwd(), 'pages/blog')))
   const articles = await getAllArticles(tags);
   res.json(articles);
   res.end();
