72d01cc905d32b1cf9efd38e0e27eb068e2c1134
Jamie Barton <jamie@notrab.dev>
2022-06-16 09:40:31 +0100

    feat: make download button more prominent

--- ui/logos.tsx
+++ ui/logos.tsx
@@ -3,7 +3,7 @@ import { MDXProvider } from '@mdx-js/react';
 import { components } from './blog/elements';
 import { Page } from './shared/Page';
 import { Meta } from '../lib/meta';
-import { Heading } from './components';
+import { Heading, Description, Button } from './components';
 import { GetInTouchSection } from './get-in-touch-section';
 import { HeroSection } from './hero-section';
 
@@ -14,9 +14,17 @@ const Logos = (): FC<Meta> =>
         <Page title={title} description={description}>
           <HeroSection hideCirclesOnMobile>
             <Heading>Logo &amp; Brand Guidelines</Heading>
+            <Description>
+              Here we've a selection of logos that you should use for
+              co-marketing.
+            </Description>
+            <Button as="a" href="static/logos.zip">
+              Download Logo Pack
+            </Button>
           </HeroSection>
           <div className="mx-auto mt-24 max-w-[790px] px-3">
             <div className="text-[#111] dark:text-[#7F818C]">{children}</div>
+
             <GetInTouchSection hideCover />
           </div>
         </Page>
