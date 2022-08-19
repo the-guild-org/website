0f59b56e70fedd6389937ea6dd953dada9745f7c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-19 15:38:54 +0200

    fix

--- lib/projects.tsx
+++ lib/projects.tsx
@@ -1,4 +1,18 @@
 export const projects = {
+  envelop: {
+    featured: true,
+    title: 'Envelop: The missing GraphQL plugin system',
+    image: 'https://envelop.dev/assets/home-claw.png',
+    link: 'https://envelop.dev/',
+    description: (
+      <>
+        <p>
+          Develop and share plugins that are usable with any GraphQL server
+          framework or schema.
+        </p>
+      </>
+    ),
+  },
   codegen: {
     featured: true,
     title: 'GraphQL Code Generator',
@@ -18,16 +32,34 @@ export const projects = {
       </>
     ),
   },
-  envelop: {
+  mesh: {
     featured: true,
-    title: 'Envelop: The missing GraphQL plugin system',
-    image: 'https://envelop.dev/assets/home-claw.png',
-    link: 'https://envelop.dev/',
+    title: 'GraphQL-Mesh',
+    image: 'https://graphql-mesh.com/assets/subheader-logo.svg',
+    link: 'https://graphql-mesh.com',
+    description: (
+      <p>
+        GraphQL Mesh allows you to use GraphQL query language to access data in
+        remote APIs that don&apos;t run GraphQL (and also ones that do run
+        GraphQL). It can be used as a gateway to other services, or run as a
+        local GraphQL schema that aggregates data from remote APIs.
+      </p>
+    ),
+  },
+  tools: {
+    featured: true,
+    title: 'Utilities for GraphQL',
+    image: '/img/logos/tools.svg',
+    link: 'https://graphql-tools.com',
     description: (
       <>
         <p>
-          Develop and share plugins that are usable with any GraphQL server
-          framework or schema.
+          A set of utilities to build your JavaScript GraphQL schema in a
+          concise and powerful way.
+        </p>
+        <p>
+          Use GraphQL-first philosophy, mock your API or stitch multiple GraphQL
+          Schemas
         </p>
       </>
     ),
@@ -42,25 +74,6 @@ export const projects = {
         <p>
           Fully-featured GraphQL Server with focus on easy setup, performance
           and great developer experience
-        </p>
-      </>
-    ),
-  },
-  'graphql-eslint': {
-    featured: true,
-    title: 'GraphQL-ESLint',
-    image:
-      'https://raw.githubusercontent.com/dotansimha/graphql-eslint/master/logo.png',
-    link: 'https://github.com/dotansimha/graphql-eslint',
-    description: (
-      <>
-        <p>
-          GraphQL-ESLint integrates GraphQL AST in the ESLint core (as a
-          parser).
-        </p>
-        <p>
-          Works on schema and operations, easily customizable, supports custom
-          rules, visualize lint issues in real-time in IDEs.
         </p>
       </>
     ),
@@ -83,76 +96,21 @@ export const projects = {
       </>
     ),
   },
-  mesh: {
+  eslint: {
     featured: true,
-    title: 'GraphQL-Mesh',
-    image: 'https://graphql-mesh.com/assets/subheader-logo.svg',
-    link: 'https://graphql-mesh.com',
-    description: (
-      <p>
-        GraphQL Mesh allows you to use GraphQL query language to access data in
-        remote APIs that don&apos;t run GraphQL (and also ones that do run
-        GraphQL). It can be used as a gateway to other services, or run as a
-        local GraphQL schema that aggregates data from remote APIs.
-      </p>
-    ),
-  },
-  modules: {
-    featured: true,
-    title: 'GraphQL-Modules',
-    image: '/img/logos/modules.svg',
-    link: 'https://graphql-modules.com',
-    description: (
-      <p>
-        GraphQL Modules lets you separate your backend implementation to small,
-        reusable, easy-to-implement and easy-to-test pieces.
-      </p>
-    ),
-  },
-  tools: {
-    featured: true,
-    title: 'Utilities for GraphQL',
-    image: '/img/logos/tools.svg',
-    link: 'https://graphql-tools.com',
+    title: 'GraphQL-ESLint',
+    image:
+      'https://raw.githubusercontent.com/dotansimha/graphql-eslint/master/logo.png',
+    link: 'https://github.com/dotansimha/graphql-eslint',
     description: (
       <>
         <p>
-          A set of utilities to build your JavaScript GraphQL schema in a
-          concise and powerful way.
+          GraphQL-ESLint integrates GraphQL AST in the ESLint core (as a
+          parser).
         </p>
         <p>
-          Use GraphQL-first philosophy, mock your API or stitch multiple GraphQL
-          Schemas
-        </p>
-      </>
-    ),
-  },
-  cli: {
-    featured: false,
-    title: 'GraphQL CLI',
-    image: '/img/logos/cli.svg',
-    link: 'https://github.com/Urigo/graphql-cli',
-    description: (
-      <>
-        <p>Command line tool for common GraphQL development workflows</p>
-        <p>
-          Improve your workflows. Compatible with editors and IDEs. Powerful
-          plugin system.
-        </p>
-      </>
-    ),
-  },
-  sofa: {
-    featured: false,
-    title: 'REST API from GraphQL - with SOFA',
-    image: '/img/logos/sofa.svg',
-    link: 'https://sofa-api.com',
-    description: (
-      <>
-        <p>Generate RESTful APIs from your GraphQL Server</p>
-        <p>
-          Sofa takes your GraphQL Schema, looks for available queries, mutations
-          and subscriptions and turns all of that into REST API.
+          Works on schema and operations, easily customizable, supports custom
+          rules, visualize lint issues in real-time in IDEs.
         </p>
       </>
     ),
@@ -168,63 +126,6 @@ export const projects = {
         <p>
           The easiest way to configure your development environment with your
           GraphQL Schema. Supported by most tools, editors and IDEs.
-        </p>
-      </>
-    ),
-  },
-  scalars: {
-    featured: false,
-    title: 'Collection of GraphQL Scalars',
-    image: '/img/logos/scalars.svg',
-    link: 'https://graphql-scalars.dev',
-    description: (
-      <>
-        <p>
-          A library of custom GraphQL Scalars for creating precise type-safe
-          GraphQL schemas.
-        </p>
-        <p>
-          GraphQL has a limited set of scalar types and we have found there are
-          some additional scalar types that are useful in being more precise in
-          our schemas
-        </p>
-      </>
-    ),
-  },
-  apolloAngular: {
-    featured: false,
-    title: 'Apollo-Angular: GraphQL in Angular',
-    image: '/img/logos/apollo-angular.svg',
-    link: 'https://apollo-angular.com',
-    description: (
-      <>
-        <p>
-          A fully-featured, production ready caching GraphQL client for Angular
-          and every GraphQL server.
-        </p>
-        <p>
-          Apollo Angular allows you to fetch data from your GraphQL server and
-          use it in building complex and reactive UIs using the Angular
-          framework. Apollo Angular may be used in any context that Angular may
-          be used. In the browser, in NativeScript, or in Node.js when you want
-          to do server-side rendering.
-        </p>
-      </>
-    ),
-  },
-  stencilApollo: {
-    featured: false,
-    title: 'GraphQL in WebComponents - Stencil Apollo',
-    image: '/img/logos/stencil-apollo.svg',
-    link: 'https://github.com/ardatan/stencil-apollo',
-    description: (
-      <>
-        <p>Stencil-Apollo lets you easily use GraphQL in Web Components.</p>
-        <p>
-          Stencil-Apollo has the same functionalities as its React sibling
-          React-Apollo, and the usage is really simple. You're able to take
-          advantage of an entire ecosystem of Apollo Links and Cache
-          implementations
         </p>
       </>
     ),
@@ -246,7 +147,38 @@ export const projects = {
       </>
     ),
   },
-  graphqlShield: {
+  modules: {
+    featured: true,
+    title: 'GraphQL-Modules',
+    image: '/img/logos/modules.svg',
+    link: 'https://graphql-modules.com',
+    description: (
+      <p>
+        GraphQL Modules lets you separate your backend implementation to small,
+        reusable, easy-to-implement and easy-to-test pieces.
+      </p>
+    ),
+  },
+  scalars: {
+    featured: false,
+    title: 'Collection of GraphQL Scalars',
+    image: '/img/logos/scalars.svg',
+    link: 'https://graphql-scalars.dev',
+    description: (
+      <>
+        <p>
+          A library of custom GraphQL Scalars for creating precise type-safe
+          GraphQL schemas.
+        </p>
+        <p>
+          GraphQL has a limited set of scalar types and we have found there are
+          some additional scalar types that are useful in being more precise in
+          our schemas
+        </p>
+      </>
+    ),
+  },
+  shield: {
     featured: true,
     title:
       'GraphQLShield - GraphQL Permissions Framework For Complex Authorisation Systems',
@@ -260,6 +192,57 @@ export const projects = {
           for your server. Its main focus is creating a meaningful, well-rounded
           and scalable API that will make you confident about your permission
           layer.
+        </p>
+      </>
+    ),
+  },
+  sofa: {
+    featured: false,
+    title: 'REST API from GraphQL - with SOFA',
+    image: '/img/logos/sofa.svg',
+    link: 'https://sofa-api.com',
+    description: (
+      <>
+        <p>Generate RESTful APIs from your GraphQL Server</p>
+        <p>
+          Sofa takes your GraphQL Schema, looks for available queries, mutations
+          and subscriptions and turns all of that into REST API.
+        </p>
+      </>
+    ),
+  },
+  cli: {
+    featured: false,
+    title: 'GraphQL CLI',
+    image: '/img/logos/cli.svg',
+    link: 'https://github.com/Urigo/graphql-cli',
+    description: (
+      <>
+        <p>Command line tool for common GraphQL development workflows</p>
+        <p>
+          Improve your workflows. Compatible with editors and IDEs. Powerful
+          plugin system.
+        </p>
+      </>
+    ),
+  },
+  angular: {
+    featured: false,
+    title: 'Apollo-Angular: GraphQL in Angular',
+    image: '/img/logos/apollo-angular.svg',
+    link: 'https://apollo-angular.com',
+    description: (
+      <>
+        <p>
+          A fully-featured, production ready caching GraphQL client for Angular
+          and every GraphQL server.
+        </p>
+        <p>
+          Apollo Angular allows you to fetch data from your GraphQL server and
+          use it in building complex and reactive UIs using the Angular
+          framework. Apollo Angular may be used in any context that Angular may
+          be used. In the browser, in NativeScript, or in Node.js when you want
+          to do server-side rendering.
         </p>
       </>
     ),
