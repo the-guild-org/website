8a15468f2221e130d31d41b1886b033d40a7faad
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-08-25 18:04:08 +0200

    temp

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,16 @@ import { DocsThemeConfig, useConfig } from '@theguild/components';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Callout } from 'nextra-theme-docs';
+import { NextSeo } from 'next-seo';
 import { BlogCardList, Newsletter, Video } from '@/components';
 import { MetaWithLink } from './lib/meta';
 import blogsMeta from './dist/blogs-meta.json';
 
 const SITE_NAME = 'The Guild';
+
+function ensureAbsolute(url: string): string {
+  return url.startsWith('/') ? `https://the-guild.dev${url}` : url;
+}
 
 const config: DocsThemeConfig = {
   titleSuffix: ` – ${SITE_NAME}`,
@@ -20,13 +25,21 @@ const config: DocsThemeConfig = {
   footer: false,
   footerEditLink: 'Edit this page on GitHub',
   logo: null,
-  head: () => (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content={`${SITE_NAME}: Modern API Platform and Ecosystem that scales`} />
-      <meta name="og:title" content={SITE_NAME} />
-    </>
-  ),
+  head: function Head() {
+    const { title, meta: frontMatter } = useConfig();
+    const description = frontMatter.description || `${SITE_NAME}: Modern API Platform and Ecosystem that scales`;
+    const image = frontMatter.image || '/img/ogimage.png';
+    console.log({ image }, frontMatter.image);
+    return (
+      <>
+        <meta name="og:title" content={title} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content="the-guild.dev" key="ogsitename" />
+        <NextSeo title={title} description={description} openGraph={{ images: [{ url: ensureAbsolute(image) }] }} />
+      </>
+    );
+  },
   gitTimestamp: 'Last updated on',
   defaultMenuCollapsed: true,
   feedbackLink: 'Question? Give us feedback →',
@@ -69,8 +82,8 @@ const config: DocsThemeConfig = {
   },
   components: {
     Callout,
-    Video
-  }
+    Video,
+  },
 };
 
 export default config;
