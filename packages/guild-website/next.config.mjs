cb8ada93cef2fc2a2937a67bc00fbb6a1ee8d9f9
Dotan Simha <dotansimha@gmail.com>
2022-08-29 19:43:52 +0300

    fixes and cleanup

--- packages/guild-website/next.config.mjs
+++ packages/guild-website/next.config.mjs
@@ -12,21 +12,4 @@ export default withGuildDocs({
       allowFutureImage: true,
     },
   },
-  rewrites: () => [
-    {
-      source: '/feed.xml',
-      destination: '/_next/static/feed.xml',
-    },
-    {
-      source: '/sitemap.xml',
-      destination: '/_next/static/sitemap.xml',
-    },
-  ],
-  redirects: () => [
-    {
-      source: '/chat',
-      destination: 'https://go.crisp.chat/chat/embed/?website_id=af9adec5-ddfa-4db9-a4a3-25769daf2fc2',
-      permanent: true,
-    },
-  ],
 });
