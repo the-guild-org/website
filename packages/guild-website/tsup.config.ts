a4146076eab09b95dc0e3a5556a5552718ee848d
Dotan Simha <dotansimha@gmail.com>
2022-08-29 14:02:40 +0300

    scripts and deps cleanup

---
+++ packages/guild-website/tsup.config.ts
@@ -0,0 +1,24 @@
+import { basename } from 'path';
+import { defineConfig } from 'tsup'
+
+const [, , filePath] = process.argv;
+const outPath = `./dist/` + basename(filePath).replace(".ts", ".mjs");
+
+export default defineConfig({
+  entry: [filePath],
+  outDir: 'dist',
+  async onSuccess() {
+    console.info(`✅ Build done, loading script "${outPath}"...`)
+    await import(outPath);
+  },
+  platform: 'node',
+  target: "node18",
+  format: 'esm',
+  bundle: true,
+  banner: {
+    js: [
+      'import { createRequire as topLevelCreateRequire } from "module";',
+      'const require = topLevelCreateRequire(import.meta.url);',
+    ].join('\n'),
+  },
+});
