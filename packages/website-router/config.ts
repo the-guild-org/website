70265f3b89477b07e7a1bf0907cbe1485aa0482b
Dotan Simha <dotansimha@gmail.com>
2022-09-04 18:07:03 +0300

    fixes

--- packages/website-router/config.ts
+++ packages/website-router/config.ts
@@ -20,6 +20,10 @@ export const jsonConfig = {
       crispSegments: ['codegen'],
       sitemap: true,
     },
+    '/graphql/yoga': {
+      redirect: `https://the-guild.dev/graphql/yoga-server`,
+      rewrite: 301,
+    },
     '/graphql/yoga-server': {
       rewrite: `graphql-yoga.pages.dev`,
       crispSegments: ['yoga'],
