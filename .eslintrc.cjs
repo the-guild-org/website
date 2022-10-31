509fe7a9112ee8c62534f45d6252b02597dd7881
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-10-31 22:39:28 +0100

    update components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@theguild', '@theguild/eslint-config/react'],
   rules: {
     'unicorn/filename-case': 0,
+    'prefer-const': ['error', { destructuring: 'all' }],
   },
 };
