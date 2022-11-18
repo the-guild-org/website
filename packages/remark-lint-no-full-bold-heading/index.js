d4135461a6580280c516d5f2e56239a71130fe51
Tuval Simha <tuval.simha@Gmail.com>
2022-11-18 16:37:44 +0100

    Remark-lint: Fix-heading-capitalize (#936)

---
+++ packages/remark-lint-no-full-bold-heading/index.js
@@ -0,0 +1,29 @@
+import { lintRule } from 'unified-lint-rule';
+import { visit } from 'unist-util-visit';
+import { generated } from 'unist-util-generated';
+import { toString } from 'hast-util-to-string';
+
+const remarkLintNoFullBoldHeading = lintRule(
+  {
+    origin: 'remark-lint:no-full-bold-heading',
+    url: '',
+  },
+  /** @type {import('unified-lint-rule').Rule<Root, void>} */
+  (tree, file) => {
+    visit(tree, 'heading', node => {
+      if (generated(node)) {
+        return;
+      }
+      const heading = toString(node);
+      if (node.children[0].type === 'strong' && node.children[1].type === 'text') {
+        return;
+      }
+      if (node.children[0].type === 'strong') {
+        file.message(`Heading is Bold by default. Heading "${heading}" - shouldn't be bold`, node);
+      }
+    });
+  }
+);
+
+// eslint-disable-next-line import/no-default-export
+export default remarkLintNoFullBoldHeading;
