93da8de2d13bcf2985f56e696cda3492ed13f24f
Tuval Simha <tuval.simha@gmail.com>
2023-02-26 18:12:10 +0200

    fix some?
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>

--- packages/website-router/titles-tags/config.ts
+++ packages/website-router/titles-tags/config.ts
@@ -1,6 +1,6 @@
 import { HTMLTitleConfig } from './title-seo';
 
-export const MANAGE_TITLE: HTMLTitleConfig[] = [
+export const MANAGE_TITLES: HTMLTitleConfig[] = [
   {
     URL: new URL('/plugins/typescript/typescript-operations'),
     titleTagContent: 'GraphQL TypeScript',
--- packages/website-router/titles-tags/title-seo.ts
+++ packages/website-router/titles-tags/title-seo.ts
@@ -1,19 +1,31 @@
+import { MANAGE_TITLES } from './config';
+
 export interface HTMLTitleConfig {
   titleTagContent: string;
   URL: URL;
+}
+
+export function shouldHandleNewTitleChange(url: URL) {
+  if (MANAGE_TITLES.some(v => url.pathname === v.URL.pathname)) {
+    return true;
+  }
+
+  return false;
 }
 
 export class TitleHandler implements HTMLRewriterElementContentHandlers {
   constructor(private titleObjects: HTMLTitleConfig[]) {}
 
   element(element: Element) {
-    if (element.tagName === 'title') {
-      const currentURL = new URL(window.location.href).pathname;
-      const titleObject = this.titleObjects.find(obj => obj.URL === currentURL);
+    const titleObject = this.titleObjects.find(v => v.URL.pathname === element.baseURI);
 
-      if (titleObject) {
-        element.setInnerContent(titleObject.titleTagContent);
-      }
+    if (titleObject) {
+      element.append(
+        `<title>
+          ${titleObject.titleTagContent}
+        </title>`,
+        { html: true },
+      );
     }
   }
 }
