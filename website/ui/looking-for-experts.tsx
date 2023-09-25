5faf77a2d36eb9e18c80ef2d944dac7a931c1611
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-09-25 12:59:44 +0200

    GraphQLConf 2023 recap

--- website/ui/looking-for-experts.tsx
+++ website/ui/looking-for-experts.tsx
@@ -4,11 +4,10 @@ export function LookingForExperts() {
   return (
     <div className="mt-6 border-l-[3px] border-solid border-[#1cc8ee] bg-gray-100 p-6 font-bold leading-7 text-[#777] dark:bg-gray-900">
       <Link
-        href="https://graphql.org/conf/"
+        href="/blog/announcing-graphql-hive-release"
         title="Announcing GraphQL Hive, the complete GraphQL API manager"
       >
-        GraphQL Conf will take place in 19-21 of September 2023 in San-Francisco. Get your tickets
-        now!
+        Announcing GraphQL Hive, the complete GraphQL API manager
       </Link>{' '}
     </div>
   );
