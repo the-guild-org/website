9d4fd77f6d1d341c2f8925dad3977267f6a0103f
Alex <53379240+AlexandraC0@users.noreply.github.com>
2023-10-31 12:20:33 +0100

    Update authors.ts - Include Nohe from Escape

--- website/ui/authors.ts
+++ website/ui/authors.ts
@@ -191,4 +191,9 @@ export const AUTHORS: Record<string, Author> = {
     link: 'https://twitter.com/chimame_rt',
     github: 'chimame',
   },
+   nohe: {
+    name: 'Nohé Hinniger-Foray',
+    link: 'https://twitter.com/NoheHf',
+    github: 'nohehf',
+  },
 };
