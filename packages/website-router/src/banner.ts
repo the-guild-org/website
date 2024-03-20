dfc7aa9b599c554b02a9f213f06e57ccb5d52c9c
YassinEldeeb <yassineldeeb94@gmail.com>
2024-03-20 17:59:12 +0200

    improve banner styling

--- packages/website-router/src/banner.ts
+++ packages/website-router/src/banner.ts
@@ -7,10 +7,10 @@ export const REBRANDING_SURVEY = `
     style="font-size: 14px;"
     href="https://966uxfqe3ss.typeform.com/to/qimBMLmb?typeform-source=t.co"
   >
-    Let us know how The Guild could be better for you by answering our survey! <svg style="width: 18px; height: 18px; margin-top: 2px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+    Help us improve by answering our survey! <svg style="width: 18px; height: 18px; margin-top: 2px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
   </a>
 </div>
-`;
+`
 
 export const GRAPHQL_CONF_2024_BANNER = `
 <div class="hidden md:block">
@@ -47,4 +47,4 @@ export const GRAPHQL_CONF_2024_BANNER = `
       <svg style="width: 18px; height: 18px;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
     </a>
   </div>
-</div>`;
+</div>`
