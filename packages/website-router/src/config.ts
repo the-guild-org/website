205dd7b61bcb617af027e67696c4b2076343da76
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2024-08-07 12:39:09 -0500

    enable graphql conf 2024 banner (#1712)
    * enable graphql conf 2024 banner
    
    * prettier

--- packages/website-router/src/config.ts
+++ packages/website-router/src/config.ts
@@ -1,3 +1,5 @@
+import { GRAPHQL_CONF_2024_BANNER } from './banner';
+
 export type RewriteRecord = {
   rewrite: string;
   crisp?: { segments: string[] };
@@ -16,7 +18,7 @@ export const jsonConfig = {
   cfFetchCacheTtl: 60,
   gaTrackingId: 'G-VN2KZS6FK4',
   crispWebsiteId: 'af9adec5-ddfa-4db9-a4a3-25769daf2fc2',
-  defaultBanner: undefined,
+  defaultBanner: GRAPHQL_CONF_2024_BANNER,
   fallbackRoute: {
     rewrite: 'guild-dev-website.pages.dev',
     crisp: {
