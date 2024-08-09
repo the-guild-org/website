1c923d2bfe27047f4834d56dd16d9b166ae09621
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-08-09 14:49:57 +0200

    fix lint

--- .remarkrc.cjs
+++ .remarkrc.cjs
@@ -20,7 +20,6 @@ const IGNORE_CASING_WORDS = [
   'vs',
   'AST',
   'SDK',
-  's',
   'DI', // Dependency Injection
   'APIs',
   'SvelteKit',
@@ -61,7 +60,6 @@ const IGNORE_CASING_WORDS = [
   'MikroORM',
   'GraphQL.js',
   'AbortController',
-  'fetch',
 ];
 
 module.exports = {
