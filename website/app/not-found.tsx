158707996e8719e9f28ba4f088c1859acd64fbca
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-30 19:36:42 +0300

    more

---
+++ website/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { NotFoundPage } from '@theguild/components';
+
+export default function Page() {
+  return (
+    <NotFoundPage>
+      <h1 className="text-5xl">404 – This page could not be found</h1>
+    </NotFoundPage>
+  );
+}
