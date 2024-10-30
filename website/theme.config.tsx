f97d7e2eae6f18cdde159b5b594bf157ed6d5d40
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-10-30 21:17:35 +0300

    [skip ci]

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -5,37 +5,16 @@ import { LinkPreview } from '@/shared/embed/link-preview';
 import { OgCard } from '@/shared/embed/og-card';
 import { StackBlitz } from '@/shared/embed/stack-blitz';
 import { Tweet } from '@/shared/embed/tweet';
-import { Callout, defineConfig, Steps, useConfig } from '@theguild/components';
+import { Callout, Steps } from '@theguild/components';
 
 function ensureAbsolute(url: string): string {
   return url.startsWith('/') ? `https://the-guild.dev${url}` : url;
 }
 
-const siteDescription = 'Modern, Open-source API Tooling and Ecosystem that scales';
-const siteName = 'The Guild';
-
 export default defineConfig({
-  backgroundColor: {
-    dark: '15,17,20',
-    light: '250,250,250',
-  },
-  docsRepositoryBase: 'https://github.com/the-guild-org/the-guild-website/tree/master/website', // base URL for the docs repository
   head: function useHead() {
-    const { frontMatter, title: pageTitle } = useConfig();
-
-    const title = `${pageTitle} (${siteName})`;
-    const {
-      description = `${siteName}: ${siteDescription}`,
-      canonical,
-      image,
-      thumbnail,
-    } = frontMatter;
     return (
       <>
-        <title>{title}</title>
-        <meta property="og:title" content={title} />
-        <meta name="description" content={description} />
-        <meta property="og:description" content={description} />
         {canonical && <link rel="canonical" href={canonical} />}
         <meta
           name="og:image"
@@ -60,8 +39,6 @@ export default defineConfig({
     StackBlitz,
     Steps,
   },
-  websiteName: 'The Guild',
-  description: siteDescription,
   search: {
     placeholder: 'Search…',
   },
