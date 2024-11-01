884c18f788093a7cbf53252c3ed2d81c1af30135
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-01 13:34:45 +0300

    more

--- website/ui/shared/embed/tweet.tsx
+++ website/ui/shared/embed/tweet.tsx
@@ -1,28 +1,25 @@
-'use client'
+'use client';
 
 import { FC } from 'react';
+import { useTheme } from '@theguild/components';
 import { Observer } from '../observer';
 import { handleTwttrLoad } from './utils';
 
 export const Tweet: FC<{
   /** Tweet link */
   tweetLink: string;
-  /** Color theme of the Tweet */
-  theme?: 'light' | 'dark';
-  /** Alignment of the Tweet */
-  align?: 'left' | 'center' | 'right';
-}> = ({
-  tweetLink,
-  theme = 'light',
-  align = 'left',
-}) => (
-  <Observer onEnter={() => handleTwttrLoad()}>
-    <div className="flex justify-center">
-      <blockquote className="twitter-tweet" data-theme={theme} data-align={align}>
-        <a href={`https://twitter.com/${tweetLink}?ref_src=twsrc%5Etfw`}>
-          {typeof window !== 'undefined' && !window.twttr && 'Loading'}
-        </a>
+}> = ({ tweetLink }) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Observer onEnter={handleTwttrLoad}>
+      <blockquote
+        className="twitter-tweet mt-6 text-center"
+        data-theme={resolvedTheme}
+        data-align="center"
+      >
+        <a href={`https://twitter.com/${tweetLink}?ref_src=twsrc%5Etfw`}>Loading...</a>
       </blockquote>
-    </div>
-  </Observer>
-);
+    </Observer>
+  );
+};
