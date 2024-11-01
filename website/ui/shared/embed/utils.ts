ac7ea5ba336f2f58b011d2890a11b9b1f109acda
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-11-01 13:13:05 +0300

    more

--- website/ui/shared/embed/utils.ts
+++ website/ui/shared/embed/utils.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-console */
 let isTwttrScriptAdded = false;
 
 export const twttrClassNames = [
@@ -14,28 +12,15 @@ const twttrEmbedScript = `
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs")
 `;
 
-const twttrLoad = () => {
-  if (
-    typeof (window as any).twttr !== 'undefined' &&
-    (window as any).twttr.widgets &&
-    typeof (window as any).twttr.widgets.load === 'function'
-  ) {
-    (window as any).twttr.widgets.load(document.getElementById('___gatsby'));
-  }
-};
-
-export const handleTwttrLoad = () => {
+export const handleTwttrLoad = (): void => {
   if (document.querySelector(twttrClassNames) !== null && !isTwttrScriptAdded) {
     createScriptTag(null, twttrEmbedScript);
     isTwttrScriptAdded = true;
   }
-  twttrLoad();
+  window.twttr?.widgets.load(document.getElementById('___gatsby'));
 };
 
-export const createScriptTag = (
-  providerEmbedUrl: string | null,
-  providerEmbedScript: string | null,
-) => {
+const createScriptTag = (providerEmbedUrl: null, providerEmbedScript: string): void => {
   const script = document.createElement('script');
 
   script.type = 'text/javascript';
@@ -49,18 +34,9 @@ export const createScriptTag = (
   }
 
   script.onerror = error => {
-    console.error(`MdxEmbedProvider ${(error as any).type}`, error);
+    // eslint-disable-next-line no-console
+    console.error('MdxEmbedProvider', error);
   };
 
   document.getElementsByTagName('head')[0].appendChild(script);
-};
-
-export const createStyleSheet = (href: string) => {
-  const link = document.createElement('link');
-
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  link.href = href;
-
-  document.getElementsByTagName('head')[0].appendChild(link);
 };
