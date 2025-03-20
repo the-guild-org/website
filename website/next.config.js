479a147e9ef78e7c6d51812ec2516372be510186
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-20 17:00:26 +0100

    Remove ./graphql-tools-v7.mdx

--- website/next.config.js
+++ website/next.config.js
@@ -21,6 +21,7 @@ export default withGuildDocs({
       '/blog/graphql-cli': '/blog/graphql-cli-is-back',
       '/open-source': '/about-us',
       // Blog posts moved to the Hive blog
+      '/blog/graphql-tools-v7': 'https://the-guild.dev/graphql/hive/blog/graphql-tools-v7',
       '/blog/announcing-graphql-hive-release': 'https://the-guild.dev/graphql/hive/blog/announcing-graphql-hive-release',
       '/blog/building-graphql-servers-in-2022': 'https://the-guild.dev/graphql/hive/blog/building-graphql-servers-in-2022',
       '/blog/introducing-graphql-eslint': 'https://the-guild.dev/graphql/hive/blog/introducing-graphql-eslint',
