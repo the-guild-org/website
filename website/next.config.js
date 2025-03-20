622431cefa863d5f5a2367f91d75fae658eafc0d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-20 17:00:32 +0100

    Remove ./a-new-year-for-schema-stitching.mdx

--- website/next.config.js
+++ website/next.config.js
@@ -21,6 +21,7 @@ export default withGuildDocs({
       '/blog/graphql-cli': '/blog/graphql-cli-is-back',
       '/open-source': '/about-us',
       // Blog posts moved to the Hive blog
+      '/blog/a-new-year-for-schema-stitching': 'https://the-guild.dev/graphql/hive/blog/a-new-year-for-schema-stitching',
       '/blog/on-demand-shared-graphql-subscriptions-with-rxjs': 'https://the-guild.dev/graphql/hive/blog/on-demand-shared-graphql-subscriptions-with-rxjs',
       '/blog/dependency-injection-library-in-graphql-modules': 'https://the-guild.dev/graphql/hive/blog/dependency-injection-library-in-graphql-modules',
       '/blog/graphql-error-handling-with-fp': 'https://the-guild.dev/graphql/hive/blog/graphql-error-handling-with-fp',
