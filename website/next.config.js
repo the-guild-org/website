470358bfb7e040c8368ce8e0e7b075c6af37f18e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-20 17:00:41 +0100

    Remove ./graphql-eslint-3.14/index.mdx

--- website/next.config.js
+++ website/next.config.js
@@ -21,6 +21,7 @@ export default withGuildDocs({
       '/blog/graphql-cli': '/blog/graphql-cli-is-back',
       '/open-source': '/about-us',
       // Blog posts moved to the Hive blog
+      '/blog/graphql-eslint-3.14': 'https://the-guild.dev/graphql/hive/blog/graphql-eslint-3.14',
       '/blog/supercharge-your-subgraph-with-hive': 'https://the-guild.dev/graphql/hive/blog/supercharge-your-subgraph-with-hive',
       '/blog/state-of-graphql-gateways-in-2023': 'https://the-guild.dev/graphql/hive/blog/state-of-graphql-gateways-in-2023',
       '/blog/introducing-envelop': 'https://the-guild.dev/graphql/hive/blog/introducing-envelop',
