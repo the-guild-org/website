5d84c70a635804b91eec1acb7a734ce8a49ce415
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-03-20 17:00:36 +0100

    Remove ./hive-introducing-schema-policy.mdx

--- website/next.config.js
+++ website/next.config.js
@@ -21,6 +21,7 @@ export default withGuildDocs({
       '/blog/graphql-cli': '/blog/graphql-cli-is-back',
       '/open-source': '/about-us',
       // Blog posts moved to the Hive blog
+      '/blog/hive-introducing-schema-policy': 'https://the-guild.dev/graphql/hive/blog/hive-introducing-schema-policy',
       '/blog/improved-security-with-graphql-armor-support-for-yoga-server-2': 'https://the-guild.dev/graphql/hive/blog/improved-security-with-graphql-armor-support-for-yoga-server-2',
       '/blog/graphql-modules-scoped-providers': 'https://the-guild.dev/graphql/hive/blog/graphql-modules-scoped-providers',
       '/blog/hive-summer-update-2023': 'https://the-guild.dev/graphql/hive/blog/hive-summer-update-2023',
