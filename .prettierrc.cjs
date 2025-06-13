9118e8f2fa815af2f9db448cd3f10d27baa3cbd8
Ben Hutton <relequestual@gmail.com>
2025-06-13 10:02:28 +0100

    Removed require wrapper in prettier config. No longer needed in V3 and can sometimes cause problems for prettier IDE extensions

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,5 +2,5 @@ const config = require('@theguild/prettier-config');
 
 module.exports = {
   ...config,
-  plugins: [...config.plugins, require('prettier-plugin-tailwindcss')],
+  plugins: [...config.plugins, 'prettier-plugin-tailwindcss'],
 };
