e48a5f8c33dc8f7f5992e5c1c5a9a9014c53d916
Laurin Quast <laurinquast@googlemail.com>
2025-09-11 22:19:44 +0200

    ff

--- website/load-hive-feed.ts
+++ website/load-hive-feed.ts
@@ -3,5 +3,12 @@ import RSSParser from 'rss-parser';
 
 const parser = new RSSParser();
 
-const feed = await parser.parseURL('https://the-guild.dev/graphql/hive/blog/feed.xml');
-fs.writeFileSync(__dirname + '/lib/hive-blog-feed.json', JSON.stringify(feed, null, 2));
+await parser
+  .parseURL('https://the-guild.dev/graphql/hive/blog/feed.xml')
+  .then(feed =>
+    fs.writeFileSync(__dirname + '/lib/hive-blog-feed.json', JSON.stringify(feed, null, 2)),
+  )
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
