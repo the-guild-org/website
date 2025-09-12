a962efe0a1c1967e27c1920462854ddec04dbe41
Laurin Quast <laurinquast@googlemail.com>
2025-09-12 10:16:18 +0200

    feat(blog): use hive feed for showing blog posts (#1863)
    * f
    
    * ff
    
    * come on it worked with bun
    
    * so prettier

---
+++ website/load-hive-feed.ts
@@ -0,0 +1,17 @@
+import * as fs from 'node:fs';
+import RSSParser from 'rss-parser';
+
+const parser = new RSSParser();
+
+await parser
+  .parseURL('https://the-guild.dev/graphql/hive/blog/feed.xml')
+  .then(feed =>
+    fs.writeFileSync(
+      import.meta.dirname + '/lib/hive-blog-feed.json',
+      JSON.stringify(feed, null, 2),
+    ),
+  )
+  .catch(err => {
+    console.log(err);
+    process.exit(1);
+  });
